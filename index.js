c420276a48243f6d2026b5c6a4d1f6030b8b7f93
Prince Costa <you@example.com>
2022-02-25 12:06:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('btn').addEventListener('click', () => {
+    const cityName = document.getElementById('input-field').value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a6926e46a00aef4ecce809473727029a`)
+        .then(res => res.json())
+        .then(data => setData(data));
+})
+
+function setData(data){
+    document.getElementById('cityName').innerText = data.name;
+    document.getElementById('temp').innerText = data.main.temp;
+    document.getElementById('condition').innerText = data.weather[0].main;
+    const icon = data.weather[0].icon;
+    document.getElementById('img').src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+}
